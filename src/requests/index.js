b82518de298bab8e2109c44b874bac81c3ebd92a
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-27 11:40:50 -0300

    :recycle: refactor: Add generic get request and handle errors

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,6 +1,28 @@
 import axios from 'axios';
+import {errors} from '../errors';
 
-export async function getAllUsers() {
-  const response = await axios.get('https://api.github.com/users');
-  return response.data;
+export function getUsers() {
+  const response = axios.get('https://api.github.com/users')
+      .then((result) => result.data)
+      .catch((error) => {
+        if (error.response) {
+          const status = error.response.status;
+          return errors[status];
+        }
+        return error.message;
+      });
+  return response;
+}
+
+export function genericGetRequest(url) {
+  const response = axios.get(url)
+      .then((result) => result.data)
+      .catch((error) => {
+        if (error.response) {
+          const status = error.response.status;
+          return errors[status];
+        }
+        return error.message;
+      });
+  return response;
 }
