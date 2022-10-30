30744023a58765161ea7ec900555d41a07836ed4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-29 23:39:27 -0300

    :sparkles: feat: Add route favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import {Navigation} from './components/Navigation';
 import {Explore} from './pages/Explore';
+import {Favorites} from './pages/Favorites';
 import {Profile} from './pages/Profile';
 import {Repos} from './pages/Repos';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route path="explore" element={<Explore />} />
-        <Route path="profile/:user" element={<Profile />} />
-        <Route path="profile/:user/repos" element={<Repos />} />
-      </Route>
-    </Routes>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/">
+          <Route path="explore" element={<Explore />} />
+          <Route path="favorites" element={<Favorites />} />
+          <Route path="profile/:user" element={<Profile />} />
+          <Route path="profile/:user/repos" element={<Repos />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
