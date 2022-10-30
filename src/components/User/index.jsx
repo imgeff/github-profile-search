5d2abe8c87c815e25a3a1463b62f9c78cfd2c5fa
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-30 01:00:03 -0300

    :lipstick: refactor: Change User component styles

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {NavLink} from 'react-router-dom';
 import './style.css';
-import {GithubLogo} from 'phosphor-react';
 
 export function User({user}) {
+  console.log(user);
   return (
     <div className="card">
-      <div className="blob"></div>
       <img src={user.avatar_url} alt="Avatar do usuário" />
-      <h2>{user.login}</h2>
-      <nav>
-        <NavLink to={`/profile/${user.login}`}>
-          <GithubLogo size={28} color="#0d1117" weight="duotone" />
-          Ver Perfil
-        </NavLink>
-      </nav>
+      <div>
+        <h2>{user.login}</h2>
+        <nav>
+          <NavLink to={`/profile/${user.login}`}>
+            Ver Perfil
+          </NavLink>
+        </nav>
+      </div>
     </div>
   );
 }
