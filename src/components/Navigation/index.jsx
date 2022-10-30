d261448a8a5a4acf6ded3c9c309b29e6487c9e77
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-29 23:38:50 -0300

    :sparkles: feat: Implements Navigation component

---
+++ src/components/Navigation/index.jsx
@@ -0,0 +1,21 @@
+import {Bookmark, Compass} from 'phosphor-react';
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './style.css';
+
+export function Navigation() {
+  const colorPurple = '#A67BF2';
+  return (
+    <nav className="navigation">
+      <NavLink to="/explore">
+        <Compass size={40} color={colorPurple} weight="duotone" />
+        <span>Explorar</span>
+      </NavLink>
+      <NavLink to="/favorites">
+        <Bookmark size={40} color={colorPurple} weight="duotone" />
+        <span>Favoritos</span>
+      </NavLink>
+    </nav>
+  );
+}
+
