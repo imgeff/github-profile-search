288784c31043de0b9cde021ca4c58fe1f7284d3b
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-03 21:55:01 -0300

    :adhesive_bandage: fix: Change root folder

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: '/dist',
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['../.test/setup.js'],
-    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: ['/.test/setup.js'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
   },
 });
